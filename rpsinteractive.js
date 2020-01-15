cc8215a571715322bd3788a01503f1996e9928a1
JNE029 <joery.neukermans@gmail.com>
2020-01-15 22:22:45 +0100

    rps.html

--- rpsinteractive.js
+++ rpsinteractive.js
@@ -1,13 +1,15 @@
 //Player interaction 
-const playerButtons = document.querySelectorAll("button");
-playerButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        alert(button.id);
-    });
-});
+let rockButton = document.querySelector("#rock");
+let paperButton = document.querySelector("#paper");
+let scissorsButton = document.querySelector("#scissors");
+let playerChoice;
 
+rockButton.addEventListener("click", roundWinner("rock", compChoice()));
+paperButton.addEventListener("click", roundWinner("paper", compChoice()));
+scissorsButton.addEventListener("click", roundWinner("scissors", compChoice()));
 
 //Computer interaction 
+//fix randomInt -> referenceError lexical 
 let randomInt = Math.floor(Math.random() * Math.floor(3));
 function compChoice() {
     if (randomInt === 1) {
@@ -21,8 +23,24 @@ function compChoice() {
 
 
 //Actual game
+const outcome = {
+    rock: {lose: "paper", win: "scissors"},
+    paper: {lose: "scissors", win: "rock"},
+    scissors: {lose: "rock", win: "paper"}
+}
 
+function roundWinner() {
+    if (outcome[playerChoice].win === compChoice()) {
+        alert("You win!");
+        return true;
+    } else if (outcome[playerChoice].lose === compChoice()) {
+        alert("Computer wins!");
+        return false;
+    } else {
+        alert("Tie!");
+    }
+}
 
 //Testing
 console.log(compChoice());
-console.log(playerButtons);
+console.log(playerchoice);
