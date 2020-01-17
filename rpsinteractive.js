f1fe6dff15d0a55ed866c12c27bf436d9e2ef0cb
JNE029 <joery.neukermans@gmail.com>
2020-01-17 16:34:00 +0100

    rps.html

--- rpsinteractive.js
+++ rpsinteractive.js
@@ -1,51 +1,22 @@
 let playerChoice;
 let compChoice;
 
-//fix compChoice return
-function checkSelection() {
+function playerSelection() {
     const playerBtns = document.querySelectorAll("button");
     playerBtns.forEach((button) => {
         button.addEventListener("click", (e) => {
-            let playerChoice = button.id; 
-            //loads compChoice after the EventListener
-            function compSelect() {
-                let randomInt = Math.floor(Math.random() * Math.floor(3));
-            
-                if (randomInt === 1) {
-                    let compChoice = "rock";
-                } else if (randomInt === 2) {
-                    let compChoice = "paper";
-                } else {
-                    let compChoice = "scissors";
-                }
-            } 
-        });
-    });
-}
-
-/*function checkSelection() {
-    const playerBtns = document.querySelectorAll("button");
-    playerBtns.forEach((button) => {
-        button.addEventListener("click", (e) => {
-            let playerChoice = button.id; 
-            //loads compChoice after the EventListener
-            function compChoice() {
-                let randomInt = Math.floor(Math.random() * Math.floor(3));
-            
-                if (randomInt === 1) {
-                    return "rock";
-                } else if (randomInt === 2) {
-                    return "paper";
-                } else {
-                    return "scissors";
-                }
-            } 
-            console.log(playerChoice);
-            console.log(compChoice());
+            let playerChoice = button.id;            
         });
     });  
 }
-*/
+
+function compSelection() {
+    choices = ["rock", "paper", "scissors"];
+
+    const compChoice = Math.floor(Math.random() * choices.length);
+    return choices[compChoice];
+} 
+
 function roundWinner() {
     const outcome = {
         rock: {lose: "paper", win: "scissors"},
@@ -67,8 +38,7 @@ function game() {
     let computerScore = 0;
     let totalScore = playerScore + computerScore;
 
-    checkSelection();
-
+    playerSelection();
 }
 
-checkSelection();
+game();
