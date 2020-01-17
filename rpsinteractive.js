01d2126e2a9bcd63b3862be57d4441c015c47e3d
JNE029 <joery.neukermans@gmail.com>
2020-01-17 11:46:45 +0100

    rpsinteractive.js

--- rpsinteractive.js
+++ rpsinteractive.js
@@ -1,29 +1,52 @@
-//Computer interaction 
-//fix randomInt -> referenceError lexical 
-function compChoice() {
-    let randomInt = Math.floor(Math.random() * Math.floor(3));
+let playerChoice;
+let compChoice;
 
-    if (randomInt === 1) {
-        return "rock";
-    } else if (randomInt === 2) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+//fix compChoice return
+function checkSelection() {
+    const playerBtns = document.querySelectorAll("button");
+    playerBtns.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            let playerChoice = button.id; 
+            //loads compChoice after the EventListener
+            function compSelect() {
+                let randomInt = Math.floor(Math.random() * Math.floor(3));
+            
+                if (randomInt === 1) {
+                    let compChoice = "rock";
+                } else if (randomInt === 2) {
+                    let compChoice = "paper";
+                } else {
+                    let compChoice = "scissors";
+                }
+            } 
+        });
+    });
 }
 
-//Player interaction 
-let playerChoice;
-
-const playerBtns = document.querySelectorAll("button");
-playerBtns.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        alert(button.id);
-    });
-});
-
-//Actual game
-/*function roundWinner() {
+/*function checkSelection() {
+    const playerBtns = document.querySelectorAll("button");
+    playerBtns.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            let playerChoice = button.id; 
+            //loads compChoice after the EventListener
+            function compChoice() {
+                let randomInt = Math.floor(Math.random() * Math.floor(3));
+            
+                if (randomInt === 1) {
+                    return "rock";
+                } else if (randomInt === 2) {
+                    return "paper";
+                } else {
+                    return "scissors";
+                }
+            } 
+            console.log(playerChoice);
+            console.log(compChoice());
+        });
+    });  
+}
+*/
+function roundWinner() {
     const outcome = {
         rock: {lose: "paper", win: "scissors"},
         paper: {lose: "scissors", win: "rock"},
@@ -37,8 +60,15 @@ playerBtns.forEach((button) => {
     } else {
         alert("Tie!");
     }
-}*/
+}  
 
-//Testing
-console.log(compChoice());
-console.log(playerChoice);
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let totalScore = playerScore + computerScore;
+
+    checkSelection();
+
+}
+
+checkSelection();
