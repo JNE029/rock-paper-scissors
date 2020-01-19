94a2711ba5e9ee900871753f69c31f5404d86f71
JNE029 <joery.neukermans@gmail.com>
2020-01-19 12:35:32 +0100

    rpsinteractive.js

--- rpsinteractive.js
+++ rpsinteractive.js
@@ -1,40 +1,68 @@
-let playerChoice;
-let compChoice;
-
+/*
 const playerBtns = document.querySelectorAll("button");
 playerBtns.forEach((button) => {
     button.addEventListener("click", (e) => {
         let playerChoice = button.id;
     });
 });
+*/
+
+let playerRock = document.getElementById("rock");
+let playerPaper = document.getElementById("paper");
+let playerScissors = document.getElementById("scissors");
+let playerScore = 0;
+let compScore = 0;
+let totalScore = playerScore + compScore;
+
+playerRock.addEventListener("click", function() {
+    round("rock");
+});
+playerPaper.addEventListener("click", function() {
+    round("paper");
+});
+playerScissors.addEventListener("click", function() {
+    round("scissors");
+});
 
 function compSelection() {
-    choices = ["rock", "paper", "scissors"];
+    let choices = ["rock", "paper", "scissors"];
 
-    let compChoice = Math.floor(Math.random() * choices.length);
-    return choices[compChoice];
+    let compSelect = Math.floor(Math.random() * choices.length);
+    return choices[compSelect];
 } 
 
-function roundWinner() {
-    const outcome = {
-        rock: {lose: "paper", win: "scissors"},
-        paper: {lose: "scissors", win: "rock"},
-        scissors: {lose: "rock", win: "paper"}
-    }
-
-    if (outcome[playerChoice].win === compChoice()) {
-        alert("You win!");
-    } else if (outcome[playerChoice].lose === compChoice()) {
-        alert("Computer wins!");
-    } else {
-        alert("Tie!");
-    }
-}  
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let totalScore = playerScore + computerScore;
-
+function win(playerChoice, compChoice) {
+    playerScore++;
+    console.log(playerChoice + " beats " + compChoice + ", you win!");
 }
 
+function lose(playerChoice, compChoice) {
+    compScore++;
+    console.log(compChoice + " beats " + playerChoice + ", you lose!");
+}
+
+function tie(playerChoice, compChoice) {
+    console.log("Tie!");
+}
+
+function round(playerChoice) {
+    let compChoice = compSelection();
+
+    switch(playerChoice+compChoice) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            win(playerChoice, compChoice);
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            lose(playerChoice, compChoice);
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            tie(playerChoice, compChoice);
+            break;
+    }
+}
